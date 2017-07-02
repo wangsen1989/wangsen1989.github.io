7c67d19dbb85f2a056bb47c4fe8c723650839ceb
wangsen1989 <295656753@qq.com>
2017-07-02 20:53:24 +0800

    jqueryDemo

---
+++ js/index.js
@@ -0,0 +1,78 @@
+﻿$(function () {
+
+	$('#search_button').button({
+		icons : {
+			primary : 'ui-icon-search',
+		}
+	});
+	
+	$('#question_button').button({
+		icons : {
+			primary : 'ui-icon-lightbulb',
+		},
+	}).click(function () {
+		alert("提问")
+	});
+	
+	$('#error').dialog({
+		autoOpen : false,
+		modal : true,
+		closeOnEscape : false,
+		resizable : false,
+		draggable : false,
+		width : 180,
+		height : 50,
+	}).parent().find('.ui-widget-header').hide();
+	
+	
+	$('#loading').dialog({
+		autoOpen : false,
+		modal : true,
+		closeOnEscape : false,
+		resizable : false,
+		draggable : false,
+		width : 180,
+		height : 50,
+	}).parent().find('.ui-widget-header').hide();
+	
+	
+	$('#tabs').tabs();
+	
+	$('#accordion').accordion({
+		header : 'h3',
+	});
+	$.ajax({
+		url:'./data/artical.json',
+		type:'GET',
+		success:function(res){
+			console.log(res)
+		}
+	})
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
